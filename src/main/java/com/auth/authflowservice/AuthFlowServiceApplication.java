9729ad5aa24657bc4a5fe211578980ff06589602
Kon Yuen Lye <konlye.work@gmail.com>
2021-04-25 00:38:41 +0800

    Initial commit

---
+++ src/main/java/com/auth/authflowservice/AuthFlowServiceApplication.java
@@ -0,0 +1,13 @@
+package com.auth.authflowservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthFlowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthFlowServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/authflowservice/AuthFlowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.authflowservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthFlowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
